0cf93550b54be3ccd30238aa96fbfd7633a91c51
Gonzalo Benzaquen <91753390+Benzagon@users.noreply.github.com>
2023-03-27 10:13:22 -0300

    feat: input text in signup

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,6 +2,7 @@ const signup: React.FC = () => {
     return (
       <div className="bg-blue-800">
         <h1 className="text-slate-100">This is a signup page</h1>
+        <input type="text" className="bg-slate-200"></input> 
       </div>
     );
   };
