285933b22c537c160e18e564d5f655223b7a0b24
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2023-03-27 15:54:34 -0300

    Signup page and components changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Button from "@/components/button";
-import Title from "@/components/header-subheader"
+import { Title, Subtitle } from "@/components/header-subheader";
 
 export default function Home() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col gap-[20px] items-center bg-slate-800 px-20 py-20 rounded-[30px]">
-          <Title title="Hello World" subtitle="Please select an option for me" />
+          <Title title="Hello World"/>
+          <Subtitle subtitle="Please select an option for me"/>
           <div className=" flex gap-2">
             <Button label="Login" link="/login"></Button>
             <Button label="Signup" link="/signup"></Button>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,19 @@
+import Button from "@/components/button";
+import { Title } from "@/components/header-subheader";
+
 const signup: React.FC = () => {
     return (
-      <div className="bg-blue-800">
-        <h1 className="text-slate-100">This is a signup page</h1>
-        <input type="text" className="bg-slate-200"></input> 
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col gap-[20px] items-center bg-slate-800 px-20 py-20 rounded-[30px]">
+          <Title title="Signup Page"></Title>
+          
+          <input type="text" className="bg-slate-200"></input>
+          <input type="text" className="bg-slate-200"></input>
+          <div className="flex">
+            <Button label="Cancel" link="/"></Button>
+            <Button label="Send" link="/main"></Button>
+          </div>
+        </div>
       </div>
     );
   };
