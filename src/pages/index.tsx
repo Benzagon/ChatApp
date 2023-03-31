d720204965dd39eb7a362927db4efcf4a3f018a6
Benzagon <gonbenzaquen@gmail.com>
2023-03-31 14:25:43 -0300

    feat: bg image in main screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Button from "@/components/button";
 import { Title, Subtitle } from "@/components/header-subheader";
+import Dog from "../../public/dog.jpg"
 
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col gap-[20px] items-center bg-slate-800 px-20 py-20 rounded-[30px]">
-          <Title title="Hello World"/>
-          <Subtitle subtitle="Please select an option for me"/>
+      <div className="bg-[url('../../public/dog.jpg')] bg-no-repeat bg-cover bg-center flex h-screen items-center justify-left">
+        <div className="ml-96 flex flex-col gap-[10px] items-center bg-slate-800 px-20 py-20 rounded-[30px]">
+          <Title title="Welcome!"/>
           <div className=" flex gap-2">
             <Button label="Login" link="/login"></Button>
             <Button label="Signup" link="/signup"></Button>
