da4efde8915239c3ee9801634dc650a6648311f2
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2023-04-01 13:28:21 -0300

    feat: signup bg and new component ButtonStructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
-import Button from "@/components/button";
+import { Button } from "@/components/button";
 import { Title, Subtitle } from "@/components/header-subheader";
 import Dog from "../../public/dog.jpg"
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('../../public/dog.jpg')] bg-no-repeat bg-cover bg-center flex h-screen items-center justify-left">
-        <div className="ml-96 flex flex-col gap-[10px] items-center bg-slate-800 px-20 py-20 rounded-[30px]">
+      <div className="bg-[url('../../public/dog.jpg')] bg-no-repeat bg-cover bg-center flex h-screen items-center justify-center">
+        <div className="flex flex-col items-center bg-slate-800 px-20 py-20 rounded-[30px]">
           <Title title="Welcome!"/>
-          <div className=" flex gap-2">
+          <Subtitle subtitle="This is a chat app"/>
+          <Subtitle subtitle="Please login or signup to continue"/>
+          <div className="flex gap-2 mt-3">
             <Button label="Login" link="/login"></Button>
             <Button label="Signup" link="/signup"></Button>
           </div>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,11 +1,10 @@
-import Button from "@/components/button";
+import { ButtonStructure } from "@/components/button";
 import { Title, Subtitle } from "@/components/header-subheader";
 
 const signup: React.FC = () => {
     return (
-      <div className="flex h-screen items-center justify-center">
-        <img src="C:\Users\47026639\Desktop\chatapp\src\components\external\fondo.mp4"></img>
-        <div className="flex flex-col bg-slate-800 px-20 py-20 rounded-[30px]">
+      <div className="flex bg-fixed min-h-screen bg-center bg-cover bg-no-repeat items-center bg-[url(../../public/blackDog.jpg)]">
+        <div className="flex flex-col bg-slate-800 px-20 py-20 rounded-[30px] ml-32">
           <Title title="Signup Page"/>
           <Subtitle subtitle="Nombre/s y Apellido/s"/>
           <input type="text" className="bg-slate-200 mb-2"></input>
@@ -14,11 +13,8 @@ const signup: React.FC = () => {
           <Subtitle subtitle="Contraseña"/>
           <input type="text" className="bg-slate-200 mb-3"></input>
           <Subtitle subtitle="Confirmar contraseña"/>
-          <input type="text" className="bg-slate-200 mb-3"></input>
-          <div className="flex gap-8">
-            <Button label="Cancel" link="/"/>
-            <Button label="Send" link="/main"/>
-          </div>
+          <input type="text" className="bg-slate-200"></input>
+          <ButtonStructure label={["Cancel", "Send"]} link="/login" gap="gap-3" ></ButtonStructure>
         </div>
       </div>
     );
