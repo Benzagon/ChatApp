8ca174c70d0e2dc09ca06386267156f1eed2d8da
Benzagon <gonbenzaquen@gmail.com>
2023-04-02 19:02:56 -0300

    feat: added browndog to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,12 @@
 import Form from "@/components/form";
+import {Button} from "../components/button"
 
 const login: React.FC = () => {
   return (
-    <Form title="Login"/>
+    <div className="flex bg-fixed min-h-screen bg-center bg-cover bg-no-repeat items-center bg-[url(../../public/brownDog.jpg)]">
+      <h1>login</h1>
+      <Button label="Cancel" link="/"></Button>
+    </div>
   );
 };
 
