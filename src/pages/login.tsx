2e544225a3ac17f7f3893d629cd1eb330137f338
Benzagon <gonbenzaquen@gmail.com>
2023-04-02 21:20:10 -0300

    feat: basic login page html and css with axios

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,85 @@
 import Form from "@/components/form";
-import {Button} from "../components/button"
+import { Button, ButtonStructure } from "../components/button";
+import { Title, Subtitle } from "@/components/header-subheader";
+
+import { useState } from "react";
+import axios from "axios";
 
 const login: React.FC = () => {
+
+  const loginHandler = (email: string, password: string) => {
+    axios.post("http://localhost:3001/login", {
+      user: [
+        {
+          "email": email,
+          "password": password
+        }
+      ]
+    })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(error => console.error(error));
+  };
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleInput = (e: any) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+  
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e: any) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    loginHandler(formData.email, formData.password)
+    setFormData({ 
+      email: "", 
+      password: "" 
+    })
+  }
+
+
   return (
     <div className="flex bg-fixed min-h-screen bg-center bg-cover bg-no-repeat items-center bg-[url(../../public/brownDog.jpg)]">
-      <h1>login</h1>
-      <Button label="Cancel" link="/"></Button>
+      <div className="flex flex-col bg-slate-800 px-20 py-20 rounded-[30px] ml-32 mr-10">
+        <Title title="Login Page" />
+        <form onSubmit={submitForm} method="post">
+          <div className="grid">
+            <Subtitle subtitle="Email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleInput}
+              className="bg-slate-200 mb-3"
+            ></input>
+            <Subtitle subtitle="Password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleInput}
+              className="bg-slate-200 mb-3"
+            ></input>
+            <button
+              type="submit"
+              className="text-slate-300 font-bold py-2 px-4 rounded bg-blue-700 hover:bg-blue-800 transition duration-100"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
