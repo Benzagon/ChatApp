bbc2d485ed42cf2bc37ed46c12e8471789fe6800
Benzagon <gonbenzaquen@gmail.com>
2023-04-08 01:21:15 -0300

    feat: home and sidebar components

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+    return(
+        <>
+            <div className="w-96 h-screen bg-slate-200 z-10 left-0 fixed">
+            </div>
+        </>
+    )
+};
+
+export default Sidebar;
---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/Sidebar";
+
+const home = () => {
+    return (
+        <>
+            <Sidebar></Sidebar>
+            <h1>Home</h1>
+        </>
+    )
+};
+
+export default home;
